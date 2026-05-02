695f77618d1f1072538c04d56c0f9b5bc6064324
rhv9 <rahulhasmukv@gmail.com>
2026-05-02 09:10:22 +0100

    Rocket shooter particles good.

--- Aldrioh/src/Game/Components/ShipModuleComponents.h
+++ Aldrioh/src/Game/Components/ShipModuleComponents.h
@@ -1,15 +1,24 @@
 #pragma once
 #include <Math/Math.h>
 
+
+
 struct RocketShooterComponent
 {
+	enum class State : uint8_t
+	{
+		NOT_ACTIVE,
+		ACTIVE,
+		EXHAUSTED,
+	};
+
 	float timer = 1.0f;
 	float elapsedTime = 0.0f;
 	float angleFacing = 0.0f;
 	float targetAngle = 0.0f;
 	float rotationSpeed = Math::PI;
 	constexpr static float ROTATION_OFFSET = -Math::PI / 4.0f;
-	bool active = false;
+	State state = State::NOT_ACTIVE;
 
 	RocketShooterComponent() {}
 	RocketShooterComponent(const RocketShooterComponent&) = default;
--- Aldrioh/src/Game/Systems/ShipModuleSystems.cpp
+++ Aldrioh/src/Game/Systems/ShipModuleSystems.cpp
@@ -17,6 +17,36 @@
 #include <Game/GlobalLayers.h>
 #include <Game/Entity/GameEntities.h>
 
+
+ParticleTemplate particleTemplate_rocketActive = []() {
+	ParticleTemplate pt;
+	pt.beginColour = glm::vec4(1.0f, 0.5f, 0.0f, 1.0f);
+	pt.endColour = glm::vec4(1.0f, 0.5f, 0.0f, 0.9f);
+	pt.beginSize = (1.0f / 16.0f) * 2.0f;
+	pt.endSize = (1.0f / 16.0f) * 2.0f;
+	pt.life = 0.2f;
+	pt.velocity = { 0.0f, 0.0f };
+	pt.velocityVariation = { 2.0f, 0.6f };
+	pt.rotationRange = { Math::degreesToRad(-45), Math::degreesToRad(45) };
+	pt.count = 4;
+	return pt;
+	}();
+
+ParticleTemplate particleTemplate_rocketExhausted = []() {
+	ParticleTemplate pt;
+	pt.beginColour = glm::vec4(0.6f, 0.6f, 0.6f, 1.0f);
+	pt.endColour = glm::vec4(0.5f, 0.5f, 0.5f, 1.0f);
+	pt.beginSize = (1.0f / 16.0f) * 3.0f;
+	pt.endSize = (1.0f / 16.0f) * 2.0f;
+	pt.life = 1.0f;
+	pt.velocity = { 0.0f, 0.0f };
+	pt.velocityVariation = { 1.0f, 1.0f };
+	pt.rotationRange = { Math::degreesToRad(-45), Math::degreesToRad(45) };
+	pt.count = 10;
+	return pt;
+	}();
+
+
 void EntitySystem::ShipModuleSystems(Timestep ts, Scene& scene)
 {
 	auto view = scene.getRegistry().view<RocketShooterComponent>();
@@ -28,9 +58,9 @@ void EntitySystem::ShipModuleSystems(Timestep ts, Scene& scene)
 
 		rsc.elapsedTime += ts;
 
-		if (!rsc.active && rsc.elapsedTime >= rsc.timer)
+		if (rsc.state == RocketShooterComponent::State::NOT_ACTIVE && rsc.elapsedTime >= rsc.timer)
 		{
-			rsc.active = true;
+			rsc.state = RocketShooterComponent::State::ACTIVE;
 			e.RemoveComponent<RotationComponent>();
 			rsc.angleFacing = e.GetComponent<VisualComponent>().rotation - RocketShooterComponent::ROTATION_OFFSET;
 			e.GetComponent<PhysicsMovementComponent>().naturalFallOffMultiplier = 8.0f;
@@ -42,7 +72,7 @@ void EntitySystem::ShipModuleSystems(Timestep ts, Scene& scene)
 			glm::vec2 pos = e.GetTransformComponent().position;
 			rsc.targetAngle = Math::angleBetween2d(pos, mousePos);
 		}
-		else if (rsc.active && rsc.elapsedTime <= rsc.timer + 1.2f)
+		else if (rsc.state == RocketShooterComponent::State::ACTIVE && rsc.elapsedTime <= rsc.timer + 1.2f)
 		{
 			glm::vec2 mousePos = scene.GetMousePosInScene();
 			glm::vec2 pos = e.GetTransformComponent().position;
@@ -56,6 +86,27 @@ void EntitySystem::ShipModuleSystems(Timestep ts, Scene& scene)
 			auto& mcc = e.GetComponent<MoveControllerComponent>();
 			mcc.moveDir = Math::angleToNormalizedVector(rsc.angleFacing);
 			e.GetComponent<VisualComponent>().rotation = rsc.angleFacing + RocketShooterComponent::ROTATION_OFFSET;
+
+			particleTemplate_rocketActive.startPos = pos - mcc.moveDir / 2.2f;
+			particleTemplate_rocketActive.velocity = -mcc.moveDir * 4.0f;
+			scene.GetParticleManager().Emit(particleTemplate_rocketActive);
+		}
+		else if (rsc.state == RocketShooterComponent::State::ACTIVE)
+		{
+			rsc.state = RocketShooterComponent::State::EXHAUSTED;
+			// emit final particle
+
+			// Switch to fixed movement
+			auto& mcc = e.GetComponent<MoveControllerComponent>();
+			mcc.moveDir = glm::vec2{ 0 };
+			auto& pmc = e.GetComponent<PhysicsMovementComponent>();
+			pmc.resultantVelocity = mcc.moveDir * mcc.speed;
+			pmc.naturalFallOffMultiplier = 0.0f;
+
+			glm::vec2 pos = e.GetTransformComponent().position;
+			particleTemplate_rocketExhausted.startPos = pos - mcc.moveDir / 2.2f;
+			particleTemplate_rocketExhausted.velocity = -mcc.moveDir * 4.0f;
+			scene.GetParticleManager().Emit(particleTemplate_rocketExhausted);
 		}
 
 	}
